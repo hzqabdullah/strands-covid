fd2c39854ac34e94ce9835d087376f562d56bcac
hzqabdullah <hzq.abdullah@gmail.com>
2023-05-01 20:04:30 +0800

    Initial commit

--- app/src/main/java/com/strands/covid/enums/QuarterOfYear.kt
+++ app/src/main/java/com/strands/covid/enums/QuarterOfYear.kt
@@ -6,22 +6,22 @@ enum class QuarterOfYear {
     ONE {
         override fun getQuarterDate() = "03-31"
         override fun getQuarterLabel() = "Quarter 1"
-        override fun getQuarterColor() = Color.GREEN
+        override fun getQuarterColor() = Color.LTGRAY
     },
     TWO {
         override fun getQuarterDate() = "06-30"
         override fun getQuarterLabel() = "Quarter 2"
-        override fun getQuarterColor() = Color.MAGENTA
+        override fun getQuarterColor() = Color.GRAY
     },
     THREE {
         override fun getQuarterDate() = "09-30"
         override fun getQuarterLabel() = "Quarter 3"
-        override fun getQuarterColor() = Color.YELLOW
+        override fun getQuarterColor() = Color.DKGRAY
     },
     FOUR {
         override fun getQuarterDate() = "12-30"
         override fun getQuarterLabel() = "Quarter 4"
-        override fun getQuarterColor() = Color.CYAN
+        override fun getQuarterColor() = Color.BLACK
     };
 
     abstract fun getQuarterDate(): String
